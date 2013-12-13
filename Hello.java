c780507f66606e852a97347228c80f4367ac63a6
OSrepo <graziella.carta88@gmail.com>
2013-12-13 14:22:23 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class Hello {
-  
-  public void main {
-  System.out.println("Hello World!!");
-  }
-  
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
 }
